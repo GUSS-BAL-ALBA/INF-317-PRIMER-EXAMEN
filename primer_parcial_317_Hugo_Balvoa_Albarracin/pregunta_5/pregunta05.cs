12c23d2b877926e07a8c0c32a0ec704b9ab340c0
GUSS-BAL-ALBA <62040415+GUSS-BAL-ALBA@users.noreply.github.com>
2023-11-18 23:27:09 -0400

    Add files via upload

---
+++ primer_parcial_317_Hugo_Balvoa_Albarracin/pregunta_5/pregunta05.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int lim=10000;
+        static int[] lista=new int[lim];
+        static int i=0;
+
+        static void core(Object dd) {
+
+            int j;
+            int ss;
+//interlock  ref 
+            while ((j = Interlocked.Increment(ref i) - 1) < lim)
+            {
+                if (j % 2 == 0)
+                {
+                    ss = (j / 2) + 1;
+                    ss = ss * ss;
+                    lista[j] = ss + 1;
+                }
+                else {
+                    lista[j] = j + 1;
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int a=0; a < 4; a++)
+            {
+                ThreadPool.QueueUserWorkItem(core);
+            }
+            while(i<lim){
+                Thread.Sleep(100); //se da ese tiempo hasta qeue los hilos terminen 
+            }
+
+            foreach(int val in lista){
+                Console.Write(val+" ");
+
+            }
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
---
+++ primer_parcial_317_Hugo_Balvoa_Albarracin/pregunta_7/pregunta_07.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace pregunta_7_pi_con_net
+{
+    class Program
+    {
+        static int lim = 2000;
+        static double dig = 0;
+
+        static int i = 0;
+
+        static void generar_dig_pi(Object dd)
+        {
+            int j;
+            while ((j = Interlocked.Increment(ref i) - 1) < lim)
+            {//
+
+                ///Problema de Basilea, resuelto por Euler en 1735
+                dig = dig + (double)(1 / (double)((j + 1) * (j + 1)));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int a = 0; a < 4; a++)
+            {
+                ThreadPool.QueueUserWorkItem(generar_dig_pi);
+            }
+            while (i < lim)
+            {
+                Thread.Sleep(100);
+            }
+
+            dig = dig * 6;
+            dig = Math.Sqrt(dig);
+
+            Console.Write(dig);
+
+            Console.ReadKey();
+
+        }
+    }
+}
